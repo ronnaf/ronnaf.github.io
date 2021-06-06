22375849c502534c2d901336b145183713c1c126
Ronna Firmo <ronnwiseley@gmail.com>
2021-06-06 09:23:17 +0800

    feat: add social links and stuff

---
+++ src/constants/index.ts
@@ -0,0 +1,22 @@
+import { FiGithub, FiLink, FiLinkedin, FiTwitter } from 'react-icons/fi';
+
+export const levelsOfInfo = [
+  {
+    level: '1',
+    text: 'I’m a software engineer based in Iloilo City, Philippines. I’m a smol bean that’s always in front of a computer.',
+  },
+  {
+    level: '2',
+    text: 'I’m a software engineer based in Iloilo City, Philippines. I’m a smol bean that’s always in front of a computer. I have a dog named Maui and I love her so much.',
+  },
+  {
+    level: 'tmi',
+    text: 'I’m a software engineer based in Iloilo City, Philippines. I’m a smol bean that’s always in front of a computer. I have a dog named Maui and I love her so much. I used to drink coffee a lot when I was in college— like a lot. Now all I drink is hot cocoa because I bought a 1-year supply from S&R.',
+  },
+];
+
+export const socialLinks = [
+  { id: 'github', icon: FiGithub, link: 'https://github.com/ronnaf' },
+  { id: 'linkedin', icon: FiLinkedin, link: 'https://www.linkedin.com/in/ronnafx/' },
+  { id: 'twitter', icon: FiTwitter, link: 'https://twitter.com/ronnaf_' },
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,11 @@
 import { graphql } from 'gatsby';
-import { getImage, StaticImage, GatsbyImage } from 'gatsby-plugin-image';
+import { GatsbyImage, getImage, StaticImage } from 'gatsby-plugin-image';
 import React, { useRef, useState } from 'react';
 import { FiGithub, FiLink } from 'react-icons/fi';
+import { levelsOfInfo, socialLinks } from '../constants';
 import { ProjectNode } from '../models/ProjectNode';
 
-const levelsOfInfo = [
-  {
-    level: '1',
-    text: 'I’m a software engineer based in Iloilo City, Philippines. I’m a smol bean that’s always in front of a computer.',
-  },
-  {
-    level: '2',
-    text: 'I’m a software engineer based in Iloilo City, Philippines. I’m a smol bean that’s always in front of a computer. I have a dog named Maui and I love her so much.',
-  },
-  {
-    level: 'tmi',
-    text: 'I’m a software engineer based in Iloilo City, Philippines. I’m a smol bean that’s always in front of a computer. I have a dog named Maui and I love her so much. I used to drink coffee a lot when I was in college— like a lot. Now all I drink is hot cocoa because I bought a 1-year supply from S&R.',
-  },
-];
+// todo: soc
 
 const IndexPage = ({ data }) => {
   const [levelIndex, setLevelIndex] = useState(0);
@@ -30,30 +18,46 @@ const IndexPage = ({ data }) => {
   };
 
   return (
-    <div className='bg-blue'>
+    <div className="min-h-screen bg-blue">
+      {/* bottom-left floating links */}
+      <div className="fixed bottom-0 left-0 p-10">
+        <div className="space-y-2 text-white">
+          {socialLinks.map((social) => (
+            <div key={social.id} className="p-2 rounded-full hover:bg-white hover:bg-opacity-20">
+              <a href={social.link} target="_blank" rel="noopener noreferrer">
+                <social.icon size={24} />
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* bottom-right floating status */}
+      <div className="fixed bottom-0 right-0 p-10">
+        <div className="text-xs text-gray-400">version: 1.0.non-mobile-friendly</div>
+      </div>
       {/* landing section */}
-      <div ref={landingRef} className='relative w-3/4 h-screen mx-auto'>
+      <div ref={landingRef} className="relative w-3/4 h-screen mx-auto">
         {/* logo */}
-        <div className='flex justify-center w-full py-14'>
-          <button className='focus:outline-none'>
+        <div className="flex justify-center w-full py-14">
+          <button className="focus:outline-none">
             <StaticImage
-              src='../images/logo.png'
-              alt='logo'
-              loading='eager'
+              src="../images/logo.png"
+              alt="logo"
+              loading="eager"
               width={60}
               height={60}
             />
           </button>
         </div>
         {/* intro */}
-        <div className='absolute top-[35%] transform -translate-y-1/2 w-full'>
-          <div className='space-y-8'>
-            <div className='text-2xl text-white'>Hello there! 👋</div>
-            <div className='text-4xl text-white'>
+        <div className="absolute top-[35%] transform -translate-y-1/2 w-full">
+          <div className="space-y-8">
+            <div className="text-2xl text-white">Hello there! 👋</div>
+            <div className="text-4xl text-white">
               I’m <b>Ronna</b>. I build things for web and mobile.
             </div>
             <div>
-              <div className='flex mb-2 space-x-1 text-sm text-gray-400'>
+              <div className="flex mb-2 space-x-1 text-sm text-gray-400">
                 <div>Select level of info:</div>
                 {levelsOfInfo.map((item, index) => (
                   <button
@@ -66,107 +70,97 @@ const IndexPage = ({ data }) => {
                   </button>
                 ))}
               </div>
-              <div className='text-white text-base w-[50%] leading-8'>
+              <div className="text-white text-base w-[50%] leading-8">
                 {levelsOfInfo[levelIndex].text}
               </div>
             </div>
           </div>
         </div>
         {/* drawing */}
-        <div className='absolute w-[35%] right-[25%] transform translate-x-1/2 -translate-y-1/2 top-[60%] text-center'>
+        <div className="absolute w-[35%] right-[25%] transform translate-x-1/2 -translate-y-1/2 top-[60%] text-center">
           <StaticImage
-            src='../images/landing_graphics.png'
-            alt='landing_graphics'
-            loading='eager'
+            src="../images/landing_graphics.png"
+            alt="landing_graphics"
+            loading="eager"
           />
-          <div className='text-xs text-gray-500'>lol i tried 😂</div>
+          <div className="text-xs text-gray-500">lol i tried 😂</div>
         </div>
         {/* drawing overlay */}
         {/* arrow */}
         <button
-          className='absolute text-center transform -translate-x-1/2 bottom-11 left-1/2 focus:outline-none group'
+          className="absolute text-center transform -translate-x-1/2 bottom-11 left-1/2 focus:outline-none group"
           onClick={() => scrollTo(projectsRef)}>
-          <div className='px-1 mb-4 text-white group-hover:bg-yellow-200 group-hover:text-blue'>
+          <div className="px-1 mb-4 text-white group-hover:bg-yellow-200 group-hover:text-blue">
             see my stuff
           </div>
           <StaticImage
-            src='../images/arrow_down.png'
-            alt='arrow_down'
+            src="../images/arrow_down.png"
+            alt="arrow_down"
             height={45}
-            loading='eager'
+            loading="eager"
           />
         </button>
       </div>
       {/* projects section */}
-      <div ref={projectsRef} className='w-3/4 min-h-screen py-16 mx-auto'>
-        <div className='grid grid-cols-5'>
-          <div className='relative col-span-2'>
-            <div className='absolute right-0 space-y-6 text-2xl text-center transform translate-x-1/2'>
-              <div className='font-bold text-white'>things i've built</div>
+      <div ref={projectsRef} className="w-3/4 min-h-screen py-16 mx-auto">
+        <div className="grid grid-cols-5">
+          <div className="relative col-span-2">
+            <div className="absolute right-0 space-y-6 text-2xl text-center transform translate-x-1/2">
+              <div className="font-bold text-white">things i've built</div>
               <StaticImage
-                src='../images/projects_logo.png'
-                alt='projects_logo'
+                src="../images/projects_logo.png"
+                alt="projects_logo"
                 width={60}
                 height={60}
               />
             </div>
           </div>
         </div>
-        <div className='mt-20 text-white'>
+        <div className="mt-20 text-white">
           {data.allMarkdownRemark.nodes.map((node: ProjectNode, i) => {
             const project = node.frontmatter;
             const cover = getImage(node.frontmatter.cover);
             const isFirst = i === 0;
             return (
-              <div key={`${project.title}_${i}`} className='grid grid-cols-5'>
+              <div key={`${project.title}_${i}`} className="grid grid-cols-5">
                 <div
                   className={`flex justify-end col-span-2 py-8 pr-10 border-r border-gray-500 ${
                     isFirst && 'pt-32'
                   }`}>
-                  <div className='w-[70%]'>
+                  <div className="w-[70%]">
                     <GatsbyImage
-                      className='rounded-md filter brightness-75 hover:filter-none'
+                      className="rounded-md filter brightness-75 hover:filter-none"
                       image={cover}
                       alt={project.title}
                       style={{ aspectRatio: '16 / 9' }}
-                      objectPosition='center top'
+                      objectPosition="center top"
                     />
                   </div>
                 </div>
                 <div className={`col-span-3 py-12 ${isFirst && 'pt-32'}`}>
-                  <div className='relative pl-8 my-4 text-2xl font-bold'>
+                  <div className="relative pl-8 my-4 text-2xl font-bold">
                     <div>{project.title}</div>
                     {/* line that connects to the y-axis */}
-                    <div className='absolute w-6 h-[1px] bg-gray-500 top-1/2 transform -translate-y-1/2 left-0' />
+                    <div className="absolute w-6 h-[1px] bg-gray-500 top-1/2 transform -translate-y-1/2 left-0" />
                   </div>
-                  <div className='pl-8 space-y-3'>
-                    <div className='text-xl'>{project.about}</div>
-                    <div className='text-base w-[75%] text-gray-400'>
-                      {node.excerpt}
-                    </div>
-                    <div className='flex space-x-2'>
+                  <div className="pl-8 space-y-3">
+                    <div className="text-xl">{project.about}</div>
+                    <div className="text-base w-[75%] text-gray-400">{node.excerpt}</div>
+                    <div className="flex space-x-2">
                       {project.tags.map((tag) => (
-                        <div
-                          key={tag}
-                          className='text-xs text-gray-400 whitespace-nowrap'>
+                        <div key={tag} className="text-xs text-gray-400 whitespace-nowrap">
                           #{tag}
                         </div>
                       ))}
                     </div>
-                    <div className='flex space-x-4'>
+                    <div className="flex space-x-4">
                       {project.source && (
-                        <a
-                          href={project.source}
-                          target='_blank'
-                          rel='noopener noreferrer'>
+                        <a href={project.source} target="_blank" rel="noopener noreferrer">
                           <FiGithub size={20} />
                         </a>
                       )}
                       {project.site && (
-                        <a
-                          href={project.site}
-                          target='_blank'
-                          rel='noopener noreferrer'>
+                        <a href={project.site} target="_blank" rel="noopener noreferrer">
                           <FiLink size={20} />
                         </a>
                       )}
